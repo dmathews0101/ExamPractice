12f2b04c84fcc9c63f7e45a18a6f2d31a93fa033
Dawn N Mathews <dmathews0101@gmail.com>
2022-11-12 18:30:30 +0530

    Second Commit. Working on Bottom Navigation View in MCQ App in Android Studio.

--- app/src/main/java/com/example/exampractice/MainActivity.java
+++ app/src/main/java/com/example/exampractice/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.exampractice;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.FrameLayout;
 
+import com.example.exampractice.ui.AccountFragment;
+import com.example.exampractice.ui.CategoryFragment;
+import com.example.exampractice.ui.LeaderBoardFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,6 +26,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.exampractice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout main_frame;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.nav_home:
+                            setFragment(new CategoryFragment());
+                            return true;
+
+                        case R.id.nav_leaderboard:
+                            setFragment(new LeaderBoardFragment());
+                            return true;
+
+                        case R.id.nav_account:
+                            setFragment(new AccountFragment());
+                            return true;
+
+                    }
+
+                    return false;
+                }
+            };
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
@@ -29,6 +66,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_bar);
+        main_frame = findViewById(R.id.main_frame);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,19 +90,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        setFragment(new CategoryFragment());
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void setFragment(Fragment fragment)
+    {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(main_frame.getId(), fragment);
+        transaction.commit();
     }
 }
